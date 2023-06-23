bcd64abc86a0aabe0d95f387da960010f601a61d
VitaliyBolgarov <125477680+VitaliyBolgarov@users.noreply.github.com>
2023-06-23 18:34:46 +0300

    js-hw-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const markup = galleryItems.reduce(
-    (acc, { original, preview, description }) =>
-    (acc += `<li>
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `<li>
   <a class="gallery__item" href="${original}">
     <img
       class="gallery__image"
@@ -11,15 +16,12 @@ const markup = galleryItems.reduce(
       alt="${description}"
     />
   </a>
-</li>`),
-    ''
-);
-
-galleryContainer.insertAdjacentHTML('beforeend', markup);
+</li>`
+  }).join('');
+}
 
 const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
 });
 
 console.log(galleryItems);
